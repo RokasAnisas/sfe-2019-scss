0bc2bff0eaf59c8cd9f95f3d2e9ad9669a48353c
Rokas <rokas.anisas@gmail.com>
2019-11-14 13:27:03 +0200

    Webpack config public html

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
     mode: 'development',
     entry: [
         './src/index.js'
-    ]
+    ],
+    devServer: {
+        contentBase: './dist',
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, './public/index.html')
+    })]
 }
