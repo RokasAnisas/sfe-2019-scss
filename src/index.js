5c6eb51886a156852d3c30f77f691d69c5791c25
Rokas Aniss <rokas.anisas@gmail.com>
2019-11-19 09:35:52 +0200

    Dev (#1)
    * Copy from css
    
    * rename
    
    * Remove lessons
    
    * iframe
    
    * clear project
    
    * webpack config init
    
    * Webpack config public html
    
    * index.hbs
    
    * clean webpackplugin
    
    * SASS loader
    
    * App structure styling
    
    * Syle architecture to 7-1 rule
    
    * Launcher
    
    * Launcher update
    
    * Update launcher.hbs
    
    * Grid ready
    
    * Layout adjustments
    
    * Lesson launcher js
    
    * iframe
    
    * Simple link
    
    * Routing adjustments
    
    * view and grid moved to components
    
    * Layout adjust
    
    * Single entry point setup
    
    * Update css export
    
    * Removed lessons styles
    
    * Lesson view
    
    * Lessons moved to src
    
    * Lesson 1 update
    
    * Stylelint
    
    * Update webpack.config.js
    
    * normalize added
    
    * lesson layout
    
    * Update settings.json
    
    * Update .stylelintrc.json
    
    * stylelint commented out
    
    * lesson 1
    
    * App structure adjustments
    
    * lesson 1 updates
    
    * Lesson 2
    
    * Lesson 3
    
    * Lesson 4
    
    * Lesson 5
    
    * Lesson 6
    
    * Lesson 7
    
    Also lesson 6 adjustments and lessons 8 - 10 placeholders added
    
    * Update lesson-6.hbs
    
    * Lesson 8
    
    * Update lesson-grid.scss
    
    * colors adjusted
    
    * Update custom-theme.scss
    
    * Lesson 10

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import lessonLauncher from './scripts/lessonLauncher';
+import './styles/index.scss'
+
+lessonLauncher();
---
+++ src/scripts/lessonLauncher.js
@@ -0,0 +1,59 @@
+// DO NOT USE THIS AS AN EXAMPLE
+const lessonLauncher = () => {
+    const lessonGrid = document.getElementById('app-lesson-grid');
+    const lessonView = document.getElementById('app-lesson-view');
+
+    window.onhashchange = function () {
+        watchHashChange()
+    }
+
+    const watchHashChange = () => {
+        const currentLesson = location.hash.replace('#/lessons/', "")
+        const isLessonView = location.hash.includes('/lessons/')
+
+        if (isLessonView) {
+            GridViewToggle('view')
+            setActiveLesson(currentLesson)
+            setLessonTitle(currentLesson)
+        } else {
+            GridViewToggle('grid')
+            setLessonTitle()
+        }
+    }
+
+    const GridViewToggle = (value) => {
+        if (value === 'grid') {
+            lessonGrid.classList.add('is-active')
+            lessonView.classList.remove('is-active')
+        }
+        if (value === 'view') {
+            lessonGrid.classList.remove('is-active')
+            lessonView.classList.add('is-active')
+        }
+    }
+
+    const setActiveLesson = (currentLesson) => {
+        const allLessonViews = document.getElementsByClassName('app-lesson-view__lesson')
+        const activeLesson = document.getElementById(currentLesson)
+
+        Array.prototype.forEach.call(allLessonViews, function (lessonView) {
+            lessonView.classList.remove('is-active')
+            activeLesson.classList.add('is-active')
+        });
+    }
+
+    const setLessonTitle = (name) => {
+        const lessonTitleElement = document.getElementById('app-lesson-name')
+        if (name) {
+            lessonTitleElement.innerText = name.replace('-', ' ')
+            lessonTitleElement.classList.add('is-active')
+        } else {
+            lessonTitleElement.innerText = null
+            lessonTitleElement.classList.remove('is-active')
+        }
+    }
+
+    watchHashChange()
+}
+
+export default lessonLauncher;
